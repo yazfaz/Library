e6f06b9dd8cdbfedbf27ebdc582dd5ba0d044b4c
MakerSquare Student <students@themakersquare.com>
2013-09-18 16:07:15 -0500

    Redo library project

--- library.rb
+++ library.rb
@@ -1,137 +1,143 @@
+# The Library class is responsible for the actions of a physical library: storing users, books, checking books in and out. 
 class Library
-	attr_accessor :users, :books
-	def initialize
-		@users=[]
-		@books={}
-		
-	end
-	
-	def add_user(user)
-		@users << user
-	end
-	
-	def add_book(book)
-		@books[book.title] = book
+  attr_accessor :users, :books
+  
+  # Sets @users and @books to empty arrays.Both are accessible in other classes.
+  def initialize
+    @users = []
+    @books = []
+  end
 
-		# "abc"
-		# book = @books["abc"]
-		# book.title`
-	end
-	
-	def checkout (user, book_title)
-		book = @books[book_title]
-		book.due_date = Time.now + (7*24*60*60)
-		# book.due_date calls due_date in Book class
-		book.status = "checked out" 
+  # Adds book to @books array
+  def add_book(book)
+    @books << book
+  end
 
-	end
-	
-	def checkin (user, title)
-		book = @books[title]
-		book.status = "available"
-		# because of def in book_status, even when i call checkin, the books are still coming up as checked out bc it's before the due date
-		# do i need to reset this somehow? so after being checked in, books go back to being available
-	end
-	
-	def book_status(title)
-		# where do i put book in this method 
-		book = @books[title]
-		if Time.now > book.due_date
-			book.status="overdue"
-		elsif (book.due_date <= (Time.now + (7*24*60*60))) && (book.due_date >= Time.now)
-			# book.due_date <= (Time.now + (7*24*60*60)) && book.due_date <= (Time.now + (6*24*60*60)) && book.due_date <= (Time.now + (5*24*60*60))	&&book.due_date <= (Time.now + (4*24*60*60)) && book.due_date <= (Time.now + (3*24*60*60)) && book.due_date <= (Time.now + (2*24*60*60)) && book.due_date <= (Time.now + (1*24*60*60)) && book.due_date >= (Time.now + (0*24*60*60))
-				# trying to say if book is due between today and 7 days from now
-			book.status="checked out"
-		elsif Time.now > (book.due_date + (30*24*60*60))
-		    book.status="lost"
-		else
-			book.status="available"
-		end
-	end
-	
-	def overdue_books
-		# if book.status=="overdue"
-		# 	puts "#{user},#{book}"
-		# else
-		# 	puts "Nothing overdue"
-		# end
-	end
-	def book_count
-		@books.count
-	end
-	def list(user)
-	 
-	end
+  # Adds user to @users array
+  def add_user(user)
+    @users << user
+  end
 
+# If method can_rent_books? is true and book's status is available, book can be checked out
+  def checkout(user, book)
+    if user.can_rent_books? && book.status = "available"
+      book.status = "checked out"
+      book.due_date = Time.now + (7*24*60*60)
+      user.checkout_book(book)
+    else
+      puts "Sorry, that book can't be checked out."
+    end
+  end
+  
+  def checkin(user,book)
+    book.status = "available"
+    book.due_date = nil
+    book.renter = nil
+    user.books_checked_out.delete(book)
+  end
+
+  # Maps through the books_checked_out array to put books checked out per user
+  def books_per_user(user)
+    user.books_checked_out.collect do |b|
+      puts "#{b.title} is due at #{b.due_date}."
+    end
+  end
 end
 
+  
 class Book
-	attr_accessor :title, :author, :description, :status, :due_date
+  
+  attr_accessor :title, :author, :description, :status, :due_date
+  
+  #Sets @title, @author, and @description equal to user input. 
+  # 
+  # @status is available upon a book being created.
+  # 
+  # @due_date is set as nil (it will be set upon a book being checked out)
+  def initialize(title, author, description)
+    @title = title
+    @author = author
+    @description = description
+    @status = 'available'
+    @due_date = nil
+    @renter = nil
+  end
 
-	# def due_date
-	# 	@due_date
-	# end
-	# def due_date=(new_val)
-	# 	@due_date = new_val
-	# end
-
-	def initialize(title, author, description)
-	  @title=title
-	  @author=author
-	  @description=description
-	  @status = "available"
-	  @due_date = nil
-	end
-
+  def status
+    if checked_out?
+      puts "#{@title} is checked out."
+    elsif overdue?
+      puts "#{@title} is overdue."
+    elsif lost?
+      puts "#{@title} is lost."
+    else
+      puts "#{@title} is available."
+    end
+  end
+  
+  def checked_out?
+    @status = "checked out" 
+  end
+  
+  def overdue? # If the current time is greater than the book's due date, it is overdue. 
+    @status = "overdue" if (Time.now > @due_date) && !@due_date.nil?
+  end
+  # If a book is more than a month late, it is considered lost. 
+  def lost?
+   book.status="lost" if Time.now > (book.due_date + (30*24*60*60))
+  end
+ 
 end
 
+# The User class shows what books a user has checked out and if they are eligible to check out more.
+class User
+  attr_accessor :user_name, :books_checked_out
+  def initialize (user_name)
+    @user_name = user_name
+    @books_checked_out = []
+  end
 
-# book1 = Book.new('Book One', 'Jane', 'desc')
-# book2 = Book.new('Book Two', 'Jane', 'desc')
-# puts "Book 1 title: #{book1.title}"
-# puts "Book 2 title: #{book2.title}"
+  def checkout_book(book)
+    @books_checked_out << book
+  end
+  
+  def can_rent_books?
+    books_overdue? && books_count == true
+  end
 
-# # library = Library.new
-# # library.add_book(book1)
-# # library.add_book(book2)
+  def books_overdue?
+    # return false if @books_checked_out.each {|b| b.status == "overdue"}
+    @books_checked_out.each {|b| b.status != "overdue"}
+  end
 
-# puts "# of Books in library: #{library.book_count}"
+  def books_count
+    @books_checked_out.count < 2 
+  end
+ 
+  def print_overdue
+    @books_checked_out.each do |b|
+      puts "#{b.to_s} is overdue" if b.status == "overdue"
+    end 
+  end
+end
 
-# library.add_user('Bob')
+user1=User.new("Yasmin")
+book1=Book.new("title", "author", "description")
+book2=Book.new("anothertitle", "anotherauthor", "anotherdesc")
+library=Library.new
 
-# library.checkout('Bob', 'Book One')
-
-# book1_status = library.book_status('Book One')
-# puts "Status of book 1: #{book1_status}"
-
-
-# library.checkin('Bob', 'Book One')
-
-book1 = Book.new('Jurassic Park', 'Crichton', 'dinosaurs')
-book3= Book.new('Book Three', 'Joe','info') 
-# create instance of Book class, called  book 3, with title, author, descr
-library = Library.new
-library.add_book(book3)
 library.add_book(book1)
-# add book3 to Library class
-library.add_user('Jill')
-# add user to Library class
-library.checkout('Jill','Book Three')
-library.checkout('Jill', 'Jurassic Park')
-book1.due_date = Time.now
-book3_status = library.book_status('Book Three')
-  puts "Status of book 3: #{book3_status}"
-book1_status = library.book_status('Jurassic Park')
-library.checkin('Jill','Book Three')
-library.checkin('Jill', 'Jurassic Park')
-  book3_statusnew = library.book_status('Book Three')
-    puts "Status of book 3: #{book3_status}"
+library.add_book(book2)
+library.add_user(user1)
 
-  book1_statusnew = library.book_status('Jurassic Park')
-  puts "Status of book 1: #{book1_status}"
+puts book1.title
+puts user1.user_name
 
-# Need to fix 
+puts user1.can_rent_books?
 
+library.checkout(user1,book1)
+book1.status
 
+library.books_per_user(user1)
 
-
+user1.print_overdue
